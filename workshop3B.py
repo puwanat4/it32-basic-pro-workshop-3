7dfbbbc793454594547c49d1a855ea0d4731f03c
pp <mr.ssrenz@gmail.com>
2026-07-16 16:34:15 +0700

    add workshop3B

---
+++ workshop3B.py
@@ -0,0 +1,8 @@
+m = 0
+note = 0
+o = int(input("วันนี้ต้องไปเก็บเงินทั้งหมดเท่าไหร่"))
+while m<o:
+    if True: money = int(input("จำนวนเงินที่เก็บได้"))
+    m += money
+    note += 1
+print("เงินได้มาทั้งหมด: ", m,"เงินทั้งหมด", note,"ร้าน","Mission Success")
